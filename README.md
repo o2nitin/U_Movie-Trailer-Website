51b70b3873f8377032e6520ede98b42441043bb4
Nitin Kumar Singh <Nitin.Singh@ATMECS.CORP>
2016-12-07 01:49:03 +0530

    creating movie trailer website initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Project Title
+Movie Trailer Website
+
+##Getting Started
+Download or clone the repository  
+install python2.XX  
+Run the file entertainment_centre.py
---
+++ media.py
@@ -0,0 +1,13 @@
+import webbrowser  #importing webbrowser module
+
+class Movie():  #defining class Movie
+
+    #definig the constructor for the class Movie
+    def __init__(self,movie_title,movie_storyline, poster_image,trailer):
+        self.title = movie_title 
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer
+        
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
